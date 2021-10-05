bf9316093026d922d62ed3659ca2be4cca120d47
Ashutosh Prasad <ashu87639@gmail.com>
2021-10-05 11:14:39 +0530

    second commit

---
+++ src/component/Home/HomeContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AppBar from "./AppBar";
+import Cards from "../Widgets/Cards";
+
+function HomeContainer() {
+  return (
+    <div>
+      <AppBar />
+
+      <Cards />
+    </div>
+  );
+}
+
+export default HomeContainer;
---
+++ src/component/Widgets/Cards.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SingleCards from "./SingleCards";
+
+function Cards() {
+  return (
+    <div>
+      <SingleCards />
+      <SingleCards />
+      <SingleCards />
+    </div>
+  );
+}
+
+export default Cards;
